8d6cf0ddb86ffa3ad0e58171e4e20e454ccd8a42
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-23 10:18:49 +0800

    lesson20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,14 +2,33 @@ const modal = () => {
   const modal = document.querySelector(".popup");
   const buttons = document.querySelectorAll(".popup-btn");
   const closeBtn = modal.querySelector(".popup-close");
+  const modalWindow = modal.querySelector(".popup-content");
+  let count = 0;
+  let idInterval;
+
+  const modalAnimation = () => {
+    count++;
+    idInterval = requestAnimationFrame(modalAnimation);
+    if (count < 50) {
+      modalWindow.style.left = count * 15 + "px";
+    }
+  };
 
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
-      modal.style.display = "block";
+      if (document.body.clientWidth > 768) {
+        modalWindow.style.left = "0px";
+        modal.style.display = "block";
+        idInterval = requestAnimationFrame(modalAnimation);
+      } else {
+        modal.style.display = "block";
+      }
     });
   });
 
   closeBtn.addEventListener("click", () => {
+    count = 0;
+    cancelAnimationFrame(idInterval);
     modal.style.display = "none";
   });
 };
