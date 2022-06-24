49ff443fb9dc5833316c43b6323f0f15cd5fbfc7
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-24 19:42:59 +0800

    lesson21

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import timer from "./modules/timer";
+import menu from "./modules/menu";
+import modal from "./modules/modal";
+import calculator from "./modules/calculator";
+
+timer("24 june 2022");
+menu();
+modal();
+calculator();
